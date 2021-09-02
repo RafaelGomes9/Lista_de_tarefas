f20ede9086eb7cb5865b39ea3ca089f62e9e47fe
RafaelGomes5 <79657981+RafaelGomes5@users.noreply.github.com>
2021-09-02 13:56:23 -0300

    Initial commit

---
+++ app/src/main/java/com/cursoandroid/listadetarefas/model/ITarefaDAO.java
@@ -0,0 +1,23 @@
+package com.cursoandroid.listadetarefas.model;
+
+import java.util.List;
+
+public interface ITarefaDAO {
+
+
+public boolean salvar(Tarefa tarefa);
+public boolean atualizar(Tarefa tarefa);
+public boolean deletar(Tarefa tarefa);
+public List<Tarefa> listar();
+
+
+
+
+
+
+
+
+
+
+
+}
